6e75973fadf9010c94eda5313dba1482a04043c5
sametcem <50104561+sametcem@users.noreply.github.com>
2020-02-11 19:08:56 +0100

    Add files via upload

---
+++ RetakePrep2/AddWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RetakePrep2.Models;
+
+namespace RetakePrep2
+{
+    /// <summary>
+    /// Interaction logic for AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        MainWindow _mainWindow;
+        private int newID;
+
+        public AddWindow()
+        {
+            InitializeComponent();
+        }
+
+        public AddWindow(List<DEPT> depts, MainWindow main, int maxID)
+        {
+            InitializeComponent();
+            DeptComboBox.ItemsSource = depts;
+            _mainWindow = main;
+            newID = maxID + 1; 
+
+        }
+
+        private void Save_Button_OnClick_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var newEmp = new EMP
+            {
+                EMPNO = newID,
+                ENAME = FirstNameTextBox.Text,
+                JOB = JobTextBox.Text,
+                DEPTNO = ((DEPT)DeptComboBox.SelectedItem).DEPTNO
+            };
+
+            _mainWindow.AddEmp(newEmp);
+            Close();
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ RetakePrep2/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RetakePrep2;
+using RetakePrep2.Models;
+
+namespace RetakePrep2
+{
+    public partial class MainWindow : Window
+    {
+        EDbCont db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new EDbCont();
+            getSource();
+        }
+
+        public void getSource()
+        {
+            var result = db.EMPs.Include(e => e.DEPT).ToList();
+            EmpDataGrid.ItemsSource = result;
+
+        }
+
+        private void addClick(object sender, RoutedEventArgs e)
+        {
+            int maxID = 0;
+            db.EMPs.ToList().ForEach(p =>
+            {
+                if (p.EMPNO > maxID)
+                {
+                    maxID = p.EMPNO;
+                }
+            });
+            Window a1 = new AddWindow(db.DEPTs.ToList(), this, maxID);
+            a1.Show();
+        }
+
+        private void Delete_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var emp = (EMP)EmpDataGrid.SelectedItem;
+            db.EMPs.Remove(db.EMPs.ToList().First(p => p.EMPNO == emp.EMPNO));
+            db.SaveChanges();
+            getSource();
+        }
+
+        private void EmpDataGrid_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            int count = EmpDataGrid.SelectedItems.Count;
+            studentCount.Content = "You choosed " + count + " Employees";
+        }
+
+        public void AddEmp(EMP emp)
+        {
+            db.EMPs.Add(emp);
+            db.SaveChanges();
+            getSource();
+        }
+    }
+}
