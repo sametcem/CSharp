41d8018dfb0face0070ea9b8b3b028a415a69c81
sametcem <50104561+sametcem@users.noreply.github.com>
2020-02-11 15:50:55 +0100

    Add files via upload

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+
+        // Every public method in a controller is callable as an HTTP endpoint.
+        // GET: /HelloWorld/     Http Get
+
+        //        public string Index()
+        //        {
+        //            return "This is my default action...";
+        //        }
+
+        // 
+        // GET: /HelloWorld/Welcome/    Http Get
+        ///HelloWorld/Welcome?name=Rick&numtimes=4
+        //        public string Welcome(string name, int numTimes = 1)
+        //        {
+        //            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //        }
+        // /HelloWorld/Welcome/3?name=Rick
+        public string Welcome2(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
---
+++ MvcMovie/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            //  it defaults to the "Home" controller and the "Index" method specified in the template line highlighted above.
+            /*
+             * The first URL segment determines the controller class to run.
+             * So localhost:xxxx/HelloWorld maps to the HelloWorldController class.
+             * The second part of the URL segment determines the action method on the class.
+             * So localhost:xxxx/HelloWorld/Index would cause the Index method of the HelloWorldController
+             * class to run. Notice that you only had to browse to localhost:xxxx/HelloWorld and the Index method was
+             * called by default. This is because Index is the default method that will be called on a controller
+             * if a method name isn't explicitly specified.
+             *
+             * "{controller=helloWorld}/{action=Welcome}/{id?}")
+             */
+        }
+    }
+}
