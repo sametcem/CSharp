52ca21bc69a166ab9fe113cbb51c4118a5018b41
sametcem <50104561+sametcem@users.noreply.github.com>
2020-02-11 15:58:22 +0100

    Add files via upload

---
+++ Task04/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task04
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComboBox.Items.Add(new ComboBoxItem {Content = "New status"});
+;            
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TextBox == null) return;
+            var selected = ((ComboBoxItem) ComboBox.SelectedItem).Content.ToString();
+            TextBox.Text = selected;
+        }
+
+
+      
+    }
+}
