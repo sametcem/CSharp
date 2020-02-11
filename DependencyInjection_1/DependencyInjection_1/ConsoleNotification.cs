f690608309467601097458c1568140055b7ac9dd
sametcem <50104561+sametcem@users.noreply.github.com>
2020-02-11 14:22:50 +0100

    Add files via upload

---
+++ DependencyInjection_1/DependencyInjection_1/ConsoleNotification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection_1
+{
+    internal class ConsoleNotification : INotificationService
+    {
+        public void NotifyIPChanged(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyUsernameChanged(User user)
+        {
+            Console.WriteLine($"Username has been changed to: {user.Username}");
+        }
+    }
+}
---
+++ DependencyInjection_1/DependencyInjection_1/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection_1
+{
+    interface INotificationService
+    {
+        void NotifyUsernameChanged(User user);
+        void NotifyIPChanged(User user);
+    }
+}
